ee98f3c310a6b63171f917e7fee1d6b8c10fc455
Kevin D <kevin@kads24.eu>
2012-08-01 20:32:21 +0200

    Cleaned up URL handling a litte. Added localization.

--- urls.py
+++ urls.py
@@ -1,15 +1,69 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth import views
 from django.contrib.auth.decorators import login_required
+from django.views.generic import DetailView, ListView, TemplateView
 
-urlpatterns = patterns('tippspiel.views',
-    url(r'^$', 'index_page'),
+from tippspiel.models import Player, Team, Match, Tipp
 
-    url(r'^login/$', 'login_page'),
-    url(r'^logout/$', 'logout_page'),
 
-    url(r'ranking/$', 'ranking_page'),
-    url(r'^player/(?P<player_name>\w+)/$', 'player_page'),
+urlpatterns = patterns('django.contrib.auth',
+    url(r'^login/$', 'views.login', {'template_name': 'tippspiel/login.html'}),
+    url(r'^logout/$', 'views.logout_then_login'),
+)
 
-    url(r'^matchdays/$', 'matchdays_page'),
-    url(r'^matchday/(?P<matchday_number>\d+)/$', 'matchday_page'),
+urlpatterns += patterns('tippspiel.views',
+    url(
+        r'^$',
+        login_required(TemplateView.as_view(template_name='tippspiel/overview.html')),
+        name="tippspiel_overview"
+    ),
+
+    url(
+        r'^matchdays/$',
+        login_required(ListView.as_view(
+            queryset=range(1, 35),
+            context_object_name='matchdays',
+            template_name='tippspiel/matchday_list.html'
+        )),
+        name="tippspiel_matchday_list"
+    ),
+
+    url(
+        r'^matchday/(?P<matchday_number>\d+)/$',
+        'matchday_detail',
+        name="tippspiel_matchday_detail"
+    ),
+
+    url(
+        r'^matches/$',
+        login_required(ListView.as_view(
+            queryset=Match.objects.order_by('date'),
+            context_object_name='matches'
+        )),
+        name='tippspiel_match_list'
+    ),
+
+    url(
+        r'^match/(?P<pk>\d+)/$',
+        login_required(DetailView.as_view(
+            model=Match
+        )),
+        name="tippspiel_match_detail"
+    ),
+
+    url(
+        r'^ranking/$',
+        login_required(ListView.as_view(
+            queryset=Player.objects.order_by('rank'),
+            context_object_name='players',
+            template_name='tippspiel/player_list.html'
+        )),
+        name="tippspiel_player_list"
+    ),
+
+    url(
+        r'^player/(?P<player_name>\w+)/$',
+        'player_detail',
+        name="tippspiel_player_detail"
+    )
 )
--- views.py
+++ views.py
@@ -1,68 +1,16 @@
-from django.shortcuts import render_to_response, get_object_or_404
-from tippspiel.models import Player, Team, Match, Tipp
-from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 
-@login_required
-def index_page(request):
-    return render_to_response(
-        'tippspiel/index.html',
-        context_instance=RequestContext(request)
-    )
-
-
-def login_page(request):
-    return render_to_response('tippspiel/login.html')
+from tippspiel.models import Player, Team, Match, Tipp
 
 
 @login_required
-def logout_page(request):
-    logout(request)
-    return HttpResponseRedirect('/')
-
-
-@login_required
-def ranking_page(request):
-    players = Player.objects.order_by('rank')
-    return render_to_response(
-        'tippspiel/ranking.html',
-        {
-            'players': players
-        },
-        context_instance=RequestContext(request)
-    )
-
-
-@login_required
-def player_page(request, player_name):
-    from urllib import urlencode
-    p = get_object_or_404(Player, user__username=player_name)
-    return render_to_response(
-        'tippspiel/player.html',
-        {
-            'player': p,
-            'gravatar': "http://gravatar.com/avatar/%s?s=120" % p.gravatar_hash()
-        },
-        context_instance=RequestContext(request)
-    )
-
-
-@login_required
-def matchdays_page(request):
-    return render_to_response(
-        'tippspiel/matchdays.html',
-        {
-            'matchdays': range(1, 35)
-        },
-        context_instance=RequestContext(request)
-    )
-
-
-@login_required
-def matchday_page(request, matchday_number):
+def matchday_detail(request, matchday_number):
     m_nr = int(matchday_number)
+    if m_nr < 1 or m_nr > 34:
+        raise Http404
     # build a range of 10 days that ideally has the current day in the center
     m_range = range(m_nr-4, m_nr+6)
     if m_range[0] < 1:
@@ -71,11 +19,25 @@ def matchday_page(request, matchday_number):
         m_range = [x-(m_range[-1]-34) for x in m_range]
     matches = Match.objects.filter(matchday=m_nr)
     return render_to_response(
-        'tippspiel/matchday.html',
+        'tippspiel/matchday_detail.html',
         {
             'number': m_nr,
             'matchday_range': m_range,
             'matches': matches
+        },
+        context_instance=RequestContext(request)
+    )
+
+
+@login_required
+def player_detail(request, player_name):
+    from urllib import urlencode
+    p = get_object_or_404(Player, user__username=player_name)
+    return render_to_response(
+        'tippspiel/player_detail.html',
+        {
+            'player': p,
+            'gravatar': "http://gravatar.com/avatar/%s?s=120" % p.gravatar_hash()
         },
         context_instance=RequestContext(request)
     )
